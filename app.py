d69db009bc9d8c3674f7f79e0e6d2537167fd94d
DVNJNKNS <devonmjenkins3@gmail.com>
2022-12-26 20:39:57 -0500

    attempt to create windows executable file for app.py

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import os
+path = './'
 
 def init():
   count = 0
-  if not os.path.isdir("records"):
-    print("no such directory /records.\n")
-    print("creating /records directory.\n")
+  if not os.path.isdir("./records"):
+    print("no such directory records.\n")
+    print("creating records directory.\n")
     os.mkdir("records")
-  filename = input("enter a filename: \n") + ".txt"
+
+  input("press enter to continue\n")
+  filename = input("enter a filename: \n") + ".txt" 
   main(count, filename)
 
-def main(count, filename):
-  #increase count
+def main(count, filename ):
+  #create and open new file with chosen filename
   f = open(f"records/{filename}", "a")
 
   name = input("enter a name: ")
@@ -21,23 +24,19 @@ def main(count, filename):
   f.write(f"{count}.{{\n")
   f.write(f"\tname: {name}\n")
   f.write(f"  }}\n")
-  f.close()
+  f.close()#is this necessary? more testing required
 
   confirmation(f, filename, count)
 
 
 def confirmation(f, filename, count):
-  set_continue = False
   #conf = confirmation 
   conf_input = input("Would you like to continue? [y] or [n]?\n")
   
   if conf_input == 'y' or  conf_input == 'Y':
-    set_continue == True
     main(count, filename)
 
   elif conf_input == 'n' or conf_input == 'N':
-    set_continue == False 
-    #close file
     close_file(f, filename)
 
   else:
@@ -45,7 +44,6 @@ def confirmation(f, filename, count):
     confirmation(f, filename, count)
 
 def close_file(f, filename):
-  pass
   f.close()
   #open text file
   os.system(f"open records/{filename}")
