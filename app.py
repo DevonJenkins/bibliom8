c3299bd26ac80dcc0641b6892b3adaa74e65c541
DVNJNKNS <devonmjenkins3@gmail.com>
2022-12-26 22:05:31 -0500

    add help message and OS checker for windows and mac

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import platform
 path = './'
 
 def init():
@@ -8,11 +9,20 @@ def init():
     print("creating records directory.\n")
     os.mkdir("records")
 
-  input("press enter to continue\n")
+  greeting()
   filename = input("enter a filename: \n") + ".txt" 
   main(count, filename)
 
+def greeting():
+  greeting = input("press h for help. press enter to continue\n")
+
+  if greeting == "h" or greeting == "H":
+    help()
+
+
+
 def main(count, filename ):
+
   #create and open new file with chosen filename
   f = open(f"records/{filename}", "a")
 
@@ -44,9 +54,33 @@ def confirmation(f, filename, count):
     confirmation(f, filename, count)
 
 def close_file(f, filename):
+  #this function is named confusingly. It closes the file in python but then it
+  # takes the steps to open the file in terminal
+
   f.close()
-  #open text file
-  os.system(f"open records/{filename}")
+  #windows doesn't use the same file opening convention. 
+    #I'll have to check the operating system. If the operating system is  
+    # windows then i must change file opening command. 
+  check_OS(filename)
+
+def check_OS(filename):
+  my_os = platform.system()
+  print("OS in my system : ", my_os) # TODO delete this line for final production
+
+  if my_os == "Windows":
+    os.system(f"records/{filename}")
+  
+  else:
+    os.system(f"open records/{filename}")
+    #this is currently based around mac and windows assuming that most
+    #librarians aren't using linux
+
+def help(): 
+  print("here is some help, friend")
+
+  print("to cancel at any time, press ctrl-c")
+
+  greeting()
 
 init()
 
